82c6143179ddef8de38324a73df5fc0bc8aff7e8
kchabin <kchabin@swu.ac.kr>
2024-03-04 12:15:59 +0900

    Post 모델 생성 및 시간 설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    #작성 시각, 수정 시각 자동 설정
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #author: 추후 작성 예정
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
